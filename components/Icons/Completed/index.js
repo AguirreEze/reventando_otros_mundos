7dd4beec9874c38226d0a7013f2b9f28d5ed31d6
AguirreEze <aguirre.eze95@gmail.com>
2022-01-18 15:27:48 -0300

    feat Steam icon

---
+++ components/Icons/Completed/index.js
@@ -0,0 +1,5 @@
+import { styles } from "components/Icons/Completed/styles.module.scss"
+
+export default function Completed() {
+  return <div className={styles}>completed</div>
+}
---
+++ components/Icons/OnProgress/index.js
@@ -0,0 +1,5 @@
+import { styles } from "components/Icons/OnProgress/styles.module.scss"
+
+export default function Completed() {
+  return <div className={styles}>On Progress</div>
+}
