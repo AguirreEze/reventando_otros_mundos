c7c7e0533798bd80cfb0f593cecd8e542e6ef7c1
AguirreEze <aguirre.eze95@gmail.com>
2022-01-18 15:27:37 -0300

    feat Game page

--- components/Game/index.js
+++ components/Game/index.js
@@ -1,3 +1,41 @@
-export default function Game() {
-  return <h1>Place Holder</h1>
+import Completed from "components/Icons/Completed"
+import OnProgress from "components/Icons/OnProgress"
+import Steam from "components/Icons/Steam"
+import Image from "next/image"
+import styles from "./styles.module.scss"
+
+export default function Game({
+  order,
+  name,
+  completed,
+  gameCover,
+  studio,
+  gameYear,
+  steamLink,
+}) {
+  return (
+    <article className={styles.container}>
+      <section className={styles.cover}>
+        <Image alt="Game Cover" src={gameCover} height={130} width={170} />
+      </section>
+      <section className={styles.data}>
+        <h2>{name}</h2>
+        <p>Studio: {studio}</p>
+        <p>Release date: {gameYear}</p>
+        <footer className={styles.footer}>
+          {completed ? <Completed /> : <OnProgress />}
+          {steamLink && (
+            <a
+              href={steamLink}
+              target="_blank"
+              rel="noreferrer"
+              className={styles.steam}
+            >
+              <Steam />
+            </a>
+          )}
+        </footer>
+      </section>
+    </article>
+  )
 }
