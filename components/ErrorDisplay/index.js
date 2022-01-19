a92c756505dcc692a2f2bec9d370ca609ee4bea0
AguirreEze <aguirre.eze95@gmail.com>
2022-01-19 17:30:49 -0300

    fix ErrorDisplay styles

--- components/ErrorDisplay/index.js
+++ components/ErrorDisplay/index.js
@@ -9,10 +9,16 @@ export default function ErrorDisplay({ text }) {
       <style jsx>
         {`
           div {
+            margin: 10px 0;
+            padding: 5px;
             border: 3px solid red;
             width: 100%;
             color: red;
             font-size: 14px;
+            border-radius: 5px;
+          }
+          h1 {
+            margin: 0;
           }
         `}
       </style>
