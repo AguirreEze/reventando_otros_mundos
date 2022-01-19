f7e3f42a9431f56ca4ca3fc8ab0cd044c5acebf8
AguirreEze <aguirre.eze95@gmail.com>
2022-01-19 13:46:22 -0300

    feat errorHandler midleware

---
+++ middleware/errorHandler.js
@@ -0,0 +1,11 @@
+const errorHandler = (err, req, res) => {
+  console.log(err.name)
+  if (err.name === "CastError")
+    return res.status(400).send({ error: "Data Malformed" })
+  if (err.name === "ValidationError")
+    return res.status(400).send({ error: err })
+  if (err.name === "JsonWebTokenError")
+    return res.status(401).send({ error: "invalid Token" })
+}
+
+export default errorHandler
