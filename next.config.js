5166757298c725693e90bd11ba482f70e1ed615b
AguirreEze <aguirre.eze95@gmail.com>
2022-01-19 12:15:10 -0300

    feat placeholders

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["i.postimg.cc"],
+    domains: ["i.postimg.cc", "via.placeholder.com"],
   },
   env: {
     mongodburl: process.env.MONGODB_URI,
