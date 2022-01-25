9961a9f4d6037b2f2ea06682cacc78a62e790250
AguirreEze <aguirre.eze95@gmail.com>
2022-01-25 12:23:02 -0300

    feat link to adding game

--- pages/games/index.js
+++ pages/games/index.js
@@ -4,8 +4,12 @@ import Game from "components/Game"
 import styles from "pages/games/styles.module.scss"
 import axios from "axios"
 import GamePlaceHolder from "components/GamePlaceHolder"
+import { useSession } from "next-auth/react"
+import Button from "components/Button"
+import Link from "next/link"
 
 export default function Games() {
+  const { data: session } = useSession()
   const [games, setGames] = useState([])
   useEffect(() => {
     axios.get("/api/games").then((res) => setGames(res.data))
@@ -24,6 +28,13 @@ export default function Games() {
         </p>
       </header>
       <section className={styles.games}>
+        {session && (
+          <Link href="admin/add/game">
+            <a>
+              <Button>+ Add Game +</Button>
+            </a>
+          </Link>
+        )}
         <ul className={styles.list}>
           {games.length === 0 && <GamePlaceHolder />}
           {games
