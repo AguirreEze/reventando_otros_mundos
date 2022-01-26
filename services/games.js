1b955dd86e87e682ec1385488a8668eba3f32981
AguirreEze <aguirre.eze95@gmail.com>
2022-01-26 19:05:42 -0300

    feat update Game Service

--- services/games.js
+++ services/games.js
@@ -4,3 +4,8 @@ export const addGame = (data) => {
   const request = axios.post("/api/games", data)
   return request.then((res) => res.data)
 }
+
+export const updateGame = (data, id) => {
+  const request = axios.put(`/api/games/${id}`, data)
+  return request.then((res) => res.data)
+}
