4ec7da9f246e146cf877a1f28e1554b7cfe4da16
AguirreEze <aguirre.eze95@gmail.com>
2022-01-28 10:19:22 -0300

    Feat Loading logic on layout

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,12 +1,15 @@
 import styles from "./styles.module.scss"
 import Navbar from "components/Navbar"
+import Loading from "components/Loading"
+import useLoading from "hooks/useLoading"
 
 export default function Layout({ children }) {
+  const { loading } = useLoading()
   return (
     <div className={styles.background}>
       <main className={styles.view}>
         <Navbar />
-        {children}
+        {loading ? <Loading /> : children}
       </main>
     </div>
   )
