6c10517c5f8e0f7405105633e4c0e3989a0c5d21
AguirreEze <aguirre.eze95@gmail.com>
2022-01-31 17:39:37 -0300

    feat upload anime with firebase

--- components/ModalAnime/index.js
+++ components/ModalAnime/index.js
@@ -1,39 +1,69 @@
 import styles from "./styles.module.scss"
 import useField from "hooks/useField"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import ErrorDisplay from "components/ErrorDisplay"
+import { addAnime, uploadImage } from "../../firebase/client"
+import { getDownloadURL } from "firebase/storage"
+import Loading from "components/Loading"
 
 export default function ModalAnime({ show, onClose }) {
+  const [dragState, setDragState] = useState(false)
+  const [task, setTask] = useState(null)
+  const [uploading, setUploading] = useState(false)
+  const [cover, setCover] = useState("/PlaceHolder.jpg")
+
   const name = useField({ type: "text" })
-  const cover = useField({ type: "text" })
   const studio = useField({ type: "text" })
   const state = useField({ type: "text" })
   const sinopsis = useField({ type: "textarea" })
   const genre = useField({ type: "text" })
   const year = useField({ type: "number" })
   const season = useField({ type: "text" })
+  const episodes = useField({ type: "number" })
   const [error, setError] = useState("")
-  const [showCover, setShowCover] = useState()
   const [genres, setGenres] = useState([])
+
+  useEffect(() => {
+    const onProgress = () => {
+      setUploading(true)
+    }
+    const onError = (error) => {
+      setUploading(false)
+      setDragState(false)
+      setError(error.code)
+    }
+    const onComplete = () => {
+      setUploading(false)
+      setDragState(false)
+      getDownloadURL(task.snapshot.ref).then(setCover)
+    }
+    if (task) {
+      task.on("state_changed", onProgress, onError, onComplete)
+    }
+  }, [task])
 
   if (!show) return null
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log(showCover)
-    setError(10)
-    console.log(genres)
-  }
-
-  const handleShow = (e) => {
-    e.preventDefault()
-    if (
-      cover.input.value.startsWith("http://") ||
-      cover.input.value.startsWith("https://")
-    ) {
-      setShowCover(cover.input.value)
+    try {
+      await addAnime({
+        name: name.input.value,
+        cover,
+        studio: studio.input.value,
+        state: state.input.value,
+        sinopsis: sinopsis.input.value,
+        genres,
+        year: year.input.value,
+        season: season.input.value,
+        episodes: episodes.input.value || "?",
+      })
+      onClose(false)
+    } catch (err) {
+      setError(err.message)
     }
   }
+
   const addGenre = (e) => {
     e.preventDefault()
     if (genre.input.value !== "") {
@@ -41,73 +71,108 @@ export default function ModalAnime({ show, onClose }) {
       genre.reset()
     }
   }
+  const handleDragEnter = (e) => {
+    e.preventDefault()
+    setDragState(true)
+  }
+  const handleDragLeave = (e) => {
+    e.preventDefault()
+    setDragState(false)
+  }
+
+  const handleDrop = (e) => {
+    e.preventDefault()
+    const file = e.dataTransfer.files[0]
+    const task = uploadImage(file)
+    setTask(task)
+  }
+  const handleDragOver = (e) => {
+    e.preventDefault()
+  }
 
   return (
     <div className={styles.background}>
-      <section className={styles.view}>
-        <button onClick={() => onClose(false)} className={styles.close}>
-          x
-        </button>
-        <ErrorDisplay text={error} />
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <div>
-            <label name="name">game name:</label>
-            <input {...name.input} placeholder="Name" name="name" />
-          </div>
-          <div>
-            <label>cover:</label>
-            <input {...cover.input} placeholder="Cover Link" name="cover" />
-          </div>
-          <div>
-            <label name="studio">studio:</label>
-            <input {...studio.input} placeholder="Name" name="studio" />
-          </div>
-          <div>
-            <label name="State">state:</label>
-            <input {...state.input} placeholder="State" name="State" />
-          </div>
-          <div>
-            <label name="Genre">genre:</label>
-            <input {...genre.input} placeholder="Genre" name="Genre" />
-            <button onClick={addGenre} className={styles.button}>
-              add genre
+      <section
+        className={dragState ? styles.view_drag : styles.view}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+      >
+        {dragState ? (
+          uploading && <Loading />
+        ) : (
+          <>
+            <button onClick={() => onClose(false)} className={styles.close}>
+              x
             </button>
-          </div>
-          <div>
-            <span>Genres</span>
-            <ul>
-              {genres.map((e) => (
-                <li key={e}>{e}</li>
-              ))}
-            </ul>
-          </div>
+            <ErrorDisplay text={error} />
+            <form onSubmit={handleSubmit} className={styles.form}>
+              <div>
+                <label name="name">game name:</label>
+                <input {...name.input} placeholder="Name" name="name" />
+              </div>
+              <div>
+                <span>cover:</span>
+                <img src={cover} className={styles.cover} />
+              </div>
+              <div>
+                <label name="studio">studio:</label>
+                <input {...studio.input} placeholder="Name" name="studio" />
+              </div>
+              <div>
+                <label name="State">state:</label>
+                <input {...state.input} placeholder="State" name="State" />
+              </div>
+              <div>
+                <label name="Genre">genre:</label>
+                <input {...genre.input} placeholder="Genre" name="Genre" />
+                <button onClick={addGenre} className={styles.button}>
+                  add genre
+                </button>
+              </div>
+              <div>
+                <span>Genres</span>
+                <ul>
+                  {genres.map((e) => (
+                    <li key={e}>{e}</li>
+                  ))}
+                </ul>
+              </div>
 
-          <div>
-            <label name="Year">year:</label>
-            <input {...year.input} placeholder="Year" name="Year" />
-          </div>
-          <div>
-            <label name="season">season:</label>
-            <input {...season.input} placeholder="Season" name="season" />
-          </div>
-          <div>
-            <label name="Sinopsis">sinopsis:</label>
-            <textarea
-              {...sinopsis.input}
-              placeholder="Sinopsis"
-              name="Sinopsis"
-              className={styles.textarea}
-            />
-          </div>
-          <footer className={styles.panel}>
-            <button onClick={handleShow} className={styles.button}>
-              Show Cover
-            </button>
-            <button type="submit" className={styles.button}>
-              Upload
-            </button>
-          </footer>
-        </form>
+              <div>
+                <label name="Year">year:</label>
+                <input {...year.input} placeholder="Year" name="Year" />
+              </div>
+              <div>
+                <label name="season">season:</label>
+                <input {...season.input} placeholder="Season" name="season" />
+              </div>
+              <div>
+                <label name="episodes">episodes:</label>
+                <input
+                  {...episodes.input}
+                  placeholder="Episodes"
+                  name="episodes"
+                />
+              </div>
+              <div>
+                <label name="Sinopsis">sinopsis:</label>
+                <textarea
+                  {...sinopsis.input}
+                  placeholder="Sinopsis"
+                  name="Sinopsis"
+                  className={styles.textarea}
+                />
+              </div>
+              <footer className={styles.panel}>
+                <button type="submit" className={styles.button}>
+                  Upload
+                </button>
+              </footer>
+            </form>
+          </>
+        )}
       </section>
     </div>
   )
