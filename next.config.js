14b5d6e8d99f89588a4f9ba0e250c34b11555b7d
AguirreEze <aguirre.eze95@gmail.com>
2022-02-01 09:56:37 -0300

    feat domain on firebase imgs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["i.postimg.cc", "via.placeholder.com"],
+    domains: [
+      "i.postimg.cc",
+      "via.placeholder.com",
+      "firebasestorage.googleapis.com",
+    ],
   },
   env: {
     mongodburl: process.env.MONGODB_URI,
