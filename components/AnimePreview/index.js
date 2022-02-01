2d3104ee9d54112db0f8bcbbc9cd35e7d661d467
AguirreEze <aguirre.eze95@gmail.com>
2022-02-01 11:24:42 -0300

    Feat link to anime dynamic page

--- components/AnimePreview/index.js
+++ components/AnimePreview/index.js
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 import style from "./style.module.scss"
 
 export default function AnimePreview({
@@ -7,15 +8,19 @@ export default function AnimePreview({
   id,
 }) {
   return (
-    <article className={style.card}>
-      <Image
-        src={cover}
-        height={230}
-        width={200}
-        className={style.cover}
-        alt={`${name} cover`}
-      />
-      <h2 className={style.name}>{name}</h2>
-    </article>
+    <Link href={`/radio/animes/[id]`} as={`/radio/animes/${id}`}>
+      <a>
+        <article className={style.card}>
+          <Image
+            src={cover}
+            height={230}
+            width={200}
+            className={style.cover}
+            alt={`${name} cover`}
+          />
+          <h2 className={style.name}>{name}</h2>
+        </article>
+      </a>
+    </Link>
   )
 }
