882d6c5230fd4f58c22825e5e785832d2e62d819
AguirreEze <aguirre.eze95@gmail.com>
2022-02-02 18:47:52 -0300

    feat stamps for states

--- components/AnimeReview/index.js
+++ components/AnimeReview/index.js
@@ -4,9 +4,19 @@ export default function AnimeReview({
   comentary = "-",
   score = "-",
   watched = "-",
-  episodes = "?",
+  episodes,
   state,
 }) {
+  const scoreStampStyles = (score) => {
+    if (score < 4) return styles.scoreStamp__red
+    if (score < 7) return styles.scoreStamp__yellow
+    return styles.scoreStamp
+  }
+  const stateStampStyles = (state) => {
+    if (state === "viendo") return styles.stateStamp_viendo
+    if (state === "dropeada") return styles.stateStamp_dropeada
+    if (state === "completo") return styles.stateStamp_completo
+  }
   return (
     <article>
       <header className={styles.title}>
@@ -15,12 +25,12 @@ export default function AnimeReview({
       <section className={styles.card}>
         <div className={styles.score}>
           <h2 className={styles.subTitle}>score</h2>
-          <div className={styles.scoreStamp}>
+          <div className={scoreStampStyles(score)}>
             <span className={styles.scoreValue}>{score}</span>
           </div>
         </div>
         <div className={styles.state}>
-          <div className={styles.stateStamp}>{state}</div>
+          <div className={stateStampStyles(state)}>{state}</div>
           <h2 className={styles.subTitle}>vistos:</h2>
           <p className={styles.episodes}>
             {watched} / {episodes}
