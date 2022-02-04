6c0d8f7a6738b50d9bc5eecca5e9181493a7d755
AguirreEze <aguirre.eze95@gmail.com>
2022-02-04 10:00:49 -0300

    fix header on loading state

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import styles from "./styles.module.scss"
 import Navbar from "components/Navbar"
 import Loading from "components/Loading"
@@ -9,7 +10,17 @@ export default function Layout({ children }) {
     <div className={styles.background}>
       <main className={styles.view}>
         <Navbar />
-        {loading ? <Loading /> : children}
+        {loading ? (
+          <>
+            <Head>
+              <title>Loading...</title>
+              <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Loading />
+          </>
+        ) : (
+          children
+        )}
       </main>
     </div>
   )
