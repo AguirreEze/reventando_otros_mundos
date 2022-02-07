c4754ddec1ae12e9f3e9fc9a7962b3388ef5581b
AguirreEze <aguirre.eze95@gmail.com>
2022-02-07 16:32:30 -0300

    deleted unused firebase fncs

--- firebase/client.js
+++ firebase/client.js
@@ -1,14 +1,4 @@
 import { initializeApp } from "firebase/app"
-import {
-  doc,
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  getDoc,
-  updateDoc,
-  deleteDoc,
-} from "firebase/firestore"
 import { getStorage, ref, uploadBytesResumable } from "firebase/storage"
 
 const firebaseConfig = {
@@ -24,59 +14,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 
 const storage = getStorage(app)
-const db = getFirestore(app)
 
 export const uploadImage = (file) => {
   const storageRef = ref(storage, `images/${file.name}`)
   return uploadBytesResumable(storageRef, file)
-}
-
-export const getAllAnimes = () => {
-  return getDocs(collection(db, "animes")).then(({ docs }) =>
-    docs.map((doc) => {
-      const data = doc.data()
-      const id = doc.id
-      return { ...data, id }
-    })
-  )
-}
-
-export const getAnimeByID = (id) => {
-  const docRef = doc(db, "animes", id)
-  return getDoc(docRef)
-}
-
-export const addAnime = ({
-  name,
-  cover,
-  studio,
-  state,
-  sinopsis,
-  genres,
-  year,
-  season,
-  episodes,
-}) => {
-  const createdAt = new Date()
-  const data = {
-    name,
-    cover,
-    studio,
-    state,
-    sinopsis,
-    genres,
-    year,
-    season,
-    episodes,
-    createdAt: createdAt.toISOString(),
-  }
-  return addDoc(collection(db, "animes"), data)
-}
-
-export const updateAnime = (data, id) => {
-  return updateDoc(doc(db, "animes", id), data)
-}
-
-export const deleteAnime = (id) => {
-  return deleteDoc(doc(db, "animes", id))
 }
