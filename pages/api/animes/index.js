2f04d6fac2ec780500730bb72ce7ed297e3dc5ed
AguirreEze <aguirre.eze95@gmail.com>
2022-02-07 12:05:42 -0300

    feat add anime endpoint

---
+++ pages/api/animes/index.js
@@ -0,0 +1,51 @@
+import errorHandler from "middleware/errorHandler"
+import connectDB from "middleware/mongo"
+import Anime from "models/Anime"
+import { getSession } from "next-auth/react"
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const session = await getSession({ req })
+    if (!session || session.user.group !== "Admin")
+      return res.status(401).send({ error: "Unauthorized" })
+  }
+  const {
+    name,
+    cover,
+    studio,
+    state,
+    genres,
+    year,
+    season,
+    sinopsis,
+    episodes = null,
+  } = req.body
+  const createdAt = new Date()
+  try {
+    connectDB()
+  } catch (err) {
+    errorHandler(err)
+  }
+  const data = {
+    name,
+    cover,
+    studio,
+    state,
+    sinopsis,
+    genres,
+    year,
+    season,
+    episodes,
+    createdAt: createdAt.toISOString(),
+  }
+
+  const anime = new Anime(data)
+  try {
+    const savedAnime = await anime.save()
+    res.status(200).json(savedAnime)
+  } catch (err) {
+    errorHandler(err)
+  }
+}
+
+export default handler
