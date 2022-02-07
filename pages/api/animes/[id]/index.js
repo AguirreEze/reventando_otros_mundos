1c38a934046ae16dac0f1fb2f6d00364a50d4636
AguirreEze <aguirre.eze95@gmail.com>
2022-02-07 15:36:16 -0300

    feat delete Endpoint

--- pages/api/animes/[id]/index.js
+++ pages/api/animes/[id]/index.js
@@ -45,6 +45,19 @@ const handler = async (req, res) => {
       errorHandler(err, res)
     }
   }
+  if (req.method === "DELETE") {
+    const session = await getSession({ req })
+    if (!session || session.user.group !== "Admin")
+      return res.status(401).send({ error: "Unauthorized" })
+    try {
+      connectDB()
+      const { id } = req.query
+      const deletedAnime = await Anime.findByIdAndDelete(id)
+      return res.status(200).json(deletedAnime)
+    } catch (err) {
+      errorHandler(err, res)
+    }
+  }
 }
 
 export default handler
