6db92fb6df58e1edad417bdbe2e3bd1ccffe2c30
AguirreEze <aguirre.eze95@gmail.com>
2022-02-07 12:05:10 -0300

    feat anime model on mongoose

--- models/Anime.js
+++ models/Anime.js
@@ -1,15 +1,59 @@
-import * as yup from "yup"
+import { Schema, model, models } from "mongoose"
 
-const Anime = yup.object().shape({
-  name: yup.string().required(),
-  cover: yup.string().url().required(),
-  studio: yup.string().required(),
-  state: yup.string().required(),
-  genres: yup.array().required().min(1),
-  year: yup.number().required().positive().integer(),
-  season: yup.string().required(),
-  sinopsis: yup.string().required(),
-  episodes: yup.number().positive().integer().nullable(true),
+const animeSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  cover: {
+    type: String,
+    required: true,
+  },
+  studio: {
+    type: String,
+    required: true,
+  },
+  genres: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  year: {
+    type: Number,
+    required: true,
+  },
+  season: {
+    type: String,
+    required: true,
+  },
+  sinopsis: {
+    type: String,
+    required: true,
+  },
+  episodes: {
+    type: Number,
+  },
+  createdAt: {
+    type: String,
+    required: true,
+  },
+  review: {
+    type: Schema.Types.ObjectId,
+    ref: "Review",
+  },
 })
+
+animeSchema.set("toJSON", {
+  transform: (doc, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+console.log(models)
+
+const Anime = models.Anime || model("Anime", animeSchema)
 
 export default Anime
