19fbdf85d80f76499de9b58e1562d84a55f0cd3f
AguirreEze <aguirre.eze95@gmail.com>
2022-02-07 13:24:59 -0300

    feat update anime endpoint

---
+++ pages/api/animes/[id]/index.js
@@ -0,0 +1,50 @@
+import errorHandler from "middleware/errorHandler"
+import connectDB from "middleware/mongo"
+import Anime from "models/Anime"
+import { getSession } from "next-auth/react"
+
+const handler = async (req, res) => {
+  if (req.method === "PUT") {
+    const session = await getSession({ req })
+    if (!session || session.user.group !== "Admin")
+      return res.status(401).send({ error: "Unauthorized" })
+    const {
+      name,
+      cover,
+      studio,
+      state,
+      genres,
+      year,
+      season,
+      sinopsis,
+      episodes,
+    } = req.body
+    try {
+      connectDB()
+    } catch (err) {
+      errorHandler(err, res)
+    }
+
+    const data = {
+      name,
+      cover,
+      studio,
+      state,
+      sinopsis,
+      genres,
+      year,
+      season,
+      episodes,
+    }
+    const { id } = req.query
+
+    try {
+      const savedAnime = await Anime.findByIdAndUpdate(id, data)
+      return res.status(200).json(savedAnime)
+    } catch (err) {
+      errorHandler(err, res)
+    }
+  }
+}
+
+export default handler
