24f07b05408753b3dcc2a345aec24c24a616783f
AguirreEze <aguirre.eze95@gmail.com>
2022-02-08 13:30:22 -0300

    feat upload image endpoint

---
+++ pages/api/images.js
@@ -0,0 +1,26 @@
+import { getSession } from "next-auth/react"
+import cloudinary from "cloudinary"
+import errorHandler from "middleware/errorHandler"
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const session = await getSession({ req })
+    if (!session || session.user.group !== "Admin")
+      return res.status(401).send({ error: "Unauthorized" })
+  }
+  try {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    })
+    const uploadedImage = await cloudinary.v2.uploader.upload(req.body.data, {
+      upload_preset: "ReventandoOtrosMundos",
+    })
+    res.status(200).json(uploadedImage)
+  } catch (err) {
+    errorHandler(err, res)
+  }
+}
+
+export default handler
