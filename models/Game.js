5684a64c029de5497209ef35f21f909b3ba5ba0c
AguirreEze <aguirre.eze95@gmail.com>
2022-02-08 16:06:40 -0300

    fix order-> createdAt

--- models/Game.js
+++ models/Game.js
@@ -2,7 +2,7 @@ import { Schema, model, models } from "mongoose"
 
 const game = new Schema({
   name: { type: String, required: true },
-  order: { type: Number, required: true },
+  createdAt: { type: String, required: true },
   completed: { type: Boolean, required: true },
   gameCover: { type: String, required: true },
   studio: { type: String, required: true },
