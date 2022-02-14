8a6d2715073573f8a396f9ff979cfd0ef43e1a21
AguirreEze <aguirre.eze95@gmail.com>
2022-02-14 11:21:37 -0300

    fixed time

--- pages/radio/index.js
+++ pages/radio/index.js
@@ -22,7 +22,7 @@ export default function Radio({ list }) {
         <h1 className={styles.title}>Invernalia</h1>
         <p className={styles.description}>
           Estos son los animes vistos y reseñados en el programa Invernalia.
-          Domingo 23Hs GMT-3 en{" "}
+          Domingo 22Hs hora Argentina (GMT-3) en{" "}
           <a
             href="http://japan-next.blogspot.com/"
             target="_blank"
