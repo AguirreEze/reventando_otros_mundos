77ad486373329746268ce91ddff5c069010b5e94
AguirreEze <aguirre.eze95@gmail.com>
2022-02-14 12:17:39 -0300

    feat avatar component

---
+++ components/Avatar/index.js
@@ -0,0 +1,17 @@
+import Image from "next/image"
+import styles from "./styles.module.scss"
+
+export default function Avatar({ name, avatar = "/AvatarPlaceholder.jpg" }) {
+  return (
+    <li className={styles.card}>
+      <Image
+        src={avatar}
+        alt="avatar"
+        width={50}
+        height={50}
+        className={styles.avatar}
+      />
+      <span className={styles.name}>{name}</span>
+    </li>
+  )
+}
