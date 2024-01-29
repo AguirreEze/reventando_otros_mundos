657243a14096bdc2be2e77f6981fb000ac07f94a
AguirreEze <aguirre.eze95@gmail.com>
2024-01-29 14:54:26 -0300

    feat: added update game modal

--- context/ModalProvider.tsx
+++ context/ModalProvider.tsx
@@ -3,27 +3,45 @@
 import { ReactNode, createContext, useState } from "react"
 import Modal from "components/Modal"
 import GameForm from "components/GameForm"
+import { GameType } from "types"
 
-type ModalTypes = false | "GAMES"
+type ModalTypes =
+  | {
+      type: false | "ADD_GAME"
+    }
+  | {
+      type: "UPDATE_GAME"
+      payload: GameType
+    }
+
+const DEFAULT_VALUE: ModalTypes = { type: false }
 
 export const ModalContext = createContext<{
   modal: ModalTypes
   setModal: (a: ModalTypes) => void
 }>({
-  modal: false,
+  modal: DEFAULT_VALUE,
   setModal: (a) => {},
 })
 
 export default function ModalProvider({ children }: { children: ReactNode }) {
-  const [modal, setModal] = useState<ModalTypes>(false)
+  const [modal, setModal] = useState<ModalTypes>(DEFAULT_VALUE)
 
-  function modalHandler(str: ModalTypes) {
-    switch (str) {
-      case "GAMES":
+  function modalHandler(state: ModalTypes) {
+    switch (state.type) {
+      case "ADD_GAME":
         return (
-          <Modal onClose={() => setModal(false)}>
-            GAMESTOP
-            {/* <GameForm /> */}
+          <Modal onClose={() => setModal(DEFAULT_VALUE)}>
+            <GameForm onClose={() => setModal(DEFAULT_VALUE)} />
+          </Modal>
+        )
+      case "UPDATE_GAME":
+        return (
+          <Modal onClose={() => setModal(DEFAULT_VALUE)}>
+            <GameForm
+              data={state.payload}
+              onClose={() => setModal(DEFAULT_VALUE)}
+            />
           </Modal>
         )
       default:
