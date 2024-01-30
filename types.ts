97ed5ad49199552eeaae9b3e47eb4311a0b81791
AguirreEze <aguirre.eze95@gmail.com>
2024-01-30 16:45:01 -0300

    feat: review type

--- types.ts
+++ types.ts
@@ -1,16 +1,20 @@
-export type AnimeType = {
+export type AnimeReviewType = {
+  state: "dropeada" | "viendo" | "completo"
+  score: "-" | number
+  comentary: string
+  watched: number
+  episodes: number
+}
+
+export interface AnimeType extends AnimeReviewType {
   name: string
   cover: string
   studio: string
-  state: "dropeada" | "viendo" | "completo"
   genres: string[]
   year: number
   season: "winter" | "summer" | "autumn" | "spring"
   sinopsis: string
-  episodes: number
   createdAt: string
-  comentary: string
-  score: "-" | number
   watched: number
   id: string
 }
